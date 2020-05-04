fbf21527173c665b8e47ec31b03a7d85e6e289c8
sylpi10 <syl.pillet@hotmail.fr>
2020-05-04 15:59:38 +0200

    add pagination

--- src/main/java/com/sylpi/crudapp/dao/EmployeeRepository.java
+++ src/main/java/com/sylpi/crudapp/dao/EmployeeRepository.java
@@ -2,6 +2,10 @@ package com.sylpi.crudapp.dao;
 
 import com.sylpi.crudapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+// change path name
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
