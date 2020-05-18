b5a85ec4012d18a5eb0e8e1de8e78f7e9a8aedb4
sylpi10 <syl.pillet@hotmail.fr>
2020-05-18 18:48:42 +0200

    init commit

---
+++ src/main/java/com/sylpi/mountainguide/MountainguideApplication.java
@@ -0,0 +1,13 @@
+package com.sylpi.mountainguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylpi/mountainguide/MountainguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylpi.mountainguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
