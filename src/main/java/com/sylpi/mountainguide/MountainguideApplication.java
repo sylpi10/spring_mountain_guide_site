ad40e974ec51c47f1c66d0c8b8f839a240975739
sylpi10 <syl.pillet@hotmail.fr>
2020-05-27 15:08:40 +0200

    static resources test

--- src/main/java/com/sylpi/mountainguide/MountainguideApplication.java
+++ src/main/java/com/sylpi/mountainguide/MountainguideApplication.java
@@ -2,7 +2,9 @@ package com.sylpi.mountainguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,8 +15,9 @@ public class MountainguideApplication {
 		SpringApplication.run(MountainguideApplication.class, args);
 	}
 
+
 	@Configuration
-	public class WebConfig implements WebMvcConfigurer {
+	public class WebConfig implements WebMvcConfigurer{
 		@Override
 		public void addResourceHandlers(ResourceHandlerRegistry registry) {
 			registry.addResourceHandler("/**")
