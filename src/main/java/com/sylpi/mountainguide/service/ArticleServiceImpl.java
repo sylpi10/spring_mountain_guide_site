3cf3a59f8b1e7668d087a9fc4384a51eb9555656
sylpi10 <syl.pillet@hotmail.fr>
2020-07-16 13:59:42 +0200

    add new activities with pics

--- src/main/java/com/sylpi/mountainguide/service/ArticleServiceImpl.java
+++ src/main/java/com/sylpi/mountainguide/service/ArticleServiceImpl.java
@@ -5,6 +5,7 @@ import com.sylpi.mountainguide.entity.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
